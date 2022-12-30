691af3ea41642337cba7d34692b38bfc64e73320
Luis F <luisflorexpareja@gmail.com>
2022-12-29 22:33:50 -0500

    Java básico - tema 1

---
+++ Curso Java Básico/tema-1/src/com/openbootcamp/Main.java
@@ -0,0 +1,21 @@
+package com.openbootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+        long lon = 1000000000;
+        double dec = 3.14d;
+        boolean t = true;
+        boolean f = false;
+        char c = 'L';
+        String name = "Luis";
+
+        System.out.println(num);
+        System.out.println(lon);
+        System.out.println(dec);
+        System.out.println(t);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(name);
+    }
+}
